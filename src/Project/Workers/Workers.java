cb3f3a75c924e1e11adb7f745777ead627c84943
Adrian Rudnicki <adi2602@wp.pl>
2022-01-16 20:10:11 +0100

    Add basic body of Workers class.

--- src/Project/Workers/Workers.java
+++ src/Project/Workers/Workers.java
@@ -1,4 +1,31 @@
 package Project.Workers;
 
+import java.util.Random;
+
 public class Workers {
+    private long workEndTime;
+    private String name;
+    private int workerValue = 500;
+    private String[] names = {"Katerina", "Manraj", "Ayomide", "Glen", "Star", "Kirby", "Nadia", "Rogan", "Charley", "Brogan", "Darnell", "Jody", "Jocelyn", "Rory", "Neriah", "Lenny", "Kendal", "August", "Rikki", "Reagan"};
+
+    public Workers() {
+        Random rand = new Random();
+        this.name = names[rand.nextInt(names.length)];
+    }
+
+    public void SetWorkEndTime(long time) {
+        this.workEndTime = time;
+    }
+
+    public long GetWorkEndTime() {
+        return this.workEndTime;
+    }
+
+    public String GetName() {
+        return this.name;
+    }
+
+    public int GetValue() {
+        return this.workerValue;
+    }
 }
